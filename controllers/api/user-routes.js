a66463b2cb99dceecf123ad82bbc97cb84059061
Henry <henryolson@icloud.com>
2022-02-11 12:15:41 -0600

    Got handle bars working as well as basic user routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+const res = require('express/lib/response');
+const { User } = require('../../models');
+
+// get all users
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+    //expects {username: 'example', email: 'example', password: 'example'}
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//const sequelize = require('./config/connection');
-//const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-/*const sess = {
+const sess = {
   secret: 'Super secret secret',
   cookie: {},
   resave: false,
@@ -17,23 +17,23 @@ const PORT = process.env.PORT || 3001;
   store: new SequelizeStore({
     db: sequelize
   })
-};*/
+};
 
-//app.use(session(sess));
+app.use(session(sess));
 
-//const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
-//const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });
 
-//app.engine('handlebars', hbs.engine);
-//app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-//app.use(express.json());
-//app.use(express.urlencoded({ extended: false }));
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use(require('./controllers/'));
+app.use(require('./controllers'));
 
-//sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
-//});
+});
