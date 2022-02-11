dec69a5c6f185e5e77ca2775a62a97228a282345
Henry <henryolson@icloud.com>
2022-02-11 16:52:27 -0600

    Got the login and logout functionality working

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,11 +2,18 @@ const router = require('express').Router();
 const res = require('express/lib/response');
 
 router.get('/', (req, res) => {
-    res.render('homepage')
+    res.render('homepage', {
+        loggedIn: req.session.loggedIn
+    });
 })
 
 router.get('/login', (req, res) => {
+    if(req.session.loggedIn) {
+        res.redirect('/');
+        return;    
+    }
+
     res.render('login');
-})
+});
 
 module.exports = router;
