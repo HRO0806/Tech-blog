08f03b8608880bdee8486f821011713e0ca1c383
Henry <henryolson@icloud.com>
2022-02-12 12:08:52 -0600

    Built the models and api routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const res = require('express/lib/response');
-const { User } = require('../../models');
+const { User, Post, Comment, Vote } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -19,7 +19,27 @@ router.get('/:id', (req, res) => {
         attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
-        }
+        },
+        include: [
+            {
+              model: Post,
+              attributes: ['id', 'title', 'created_at']
+            },
+            {
+              model: Comment,
+              attributes: ['id', 'comment_text', 'created_at'],
+              include: {
+                model: Post,
+                attributes: ['title']
+              }
+            },
+            {
+              model: Post,
+              attributes: ['title'],
+              through: Vote,
+              as: 'voted_posts'
+            }
+        ]
     })
     .then(dbUserData => {
         if (!dbUserData) {
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,46 @@
 const router = require('express').Router();
 const res = require('express/lib/response');
+const sequelize = require('../config/connection');
+const { Post, User, Comment, Vote } = require('../models');
 
+// get all posts for homepage
 router.get('/', (req, res) => {
-    res.render('homepage', {
-        loggedIn: req.session.loggedIn
-    });
-})
+    console.log('======================');
+    Post.findAll({
+      attributes: [
+        'id',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+  
+        res.render('homepage', {
+          posts,
+          loggedIn: req.session.loggedIn
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 router.get('/login', (req, res) => {
     if(req.session.loggedIn) {
@@ -14,6 +49,17 @@ router.get('/login', (req, res) => {
     }
 
     res.render('login');
+});
+
+router.get('/Henrys', (req, res) => {
+    res.render('Henrys');
+});
+
+router.get('/louies', (req, res) => {
+    res.render('louies');
+});
+router.get('/obiwans', (req, res) => {
+    res.render('obiwans');
 });
 
 module.exports = router;
